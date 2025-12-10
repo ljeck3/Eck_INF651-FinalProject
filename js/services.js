078c14ed7244123c9be7d4c142df4d5ce46e0e73
lj-1003 <levijustin24@gmail.com>
2025-12-09 18:52:04 -0600

    Render services from json file.

--- js/services.js
+++ js/services.js
@@ -12,7 +12,27 @@ async function getServices() {
 
 //Render
 function renderServices(services) {
+
+    
+    //Test to see if getting data
+    console.log("Loading Services...")
     services.forEach((service) => {
-    console.log(`${service.service}`);
-  })
+    console.log(`   ${service.service}`);
+    })
+
+    //For real
+    const container = document.getElementById("service-list");
+    container.innerHTML = "";
+
+    services.forEach((service) => {
+        const div = document.createElement("div");
+        div.className = "service";
+        div.innerHTML = `
+        <img src="${service.image}" alt="${service.title}">
+        <h2>${service.service}</h2>
+        <p>$${service.price}</p>
+        <p>${service.description}</p>
+        `;
+        container.appendChild(div);
+    });
 }
